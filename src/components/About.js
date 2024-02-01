28fa655ba1af524d1bd969e0f4e716ba8930bb38
stellamumbi <stellamumbi736@gmail.com>
2024-02-01 18:09:52 +0300

    completed lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { image } from "../data/data";
 
-function About() {
-  return <div>About</div>;
+
+
+const About = () => {
+
+  return <div ID="about">
+
+    <h2>About Me</h2>
+    <p>Am beautiful and wonderfuly made</p>
+    <img src="https://i.imgur.com/mV8PQxj.gif"alt="I made this" />
+
+  </div>;
 }
 
 export default About;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,17 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  const links = ["home", "about", ];
+
+  // Mapping the links to create <a> elements
+  const linkA = links.map((link) => (
+    <a key={link} href={"#" + link}>
+      {link}
+    </a>
+  ));
+
+  // Returning the navigation bar with the mapped links
+  return <nav>{linkA}</nav>;
 }
 
 export default NavBar;
